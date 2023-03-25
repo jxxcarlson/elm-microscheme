2a4d8d67f8a70ce5c4d0d940aa76bbd8ba20231c
James Carlson <jxxcarlson@gmail.com>
2023-03-25 14:08:02 -0400

    export example Exprs (to be deleted later)

--- src/MicroScheme/Expr.elm
+++ src/MicroScheme/Expr.elm
@@ -1,4 +1,4 @@
-module MicroScheme.Expr exposing (Expr(..))
+module MicroScheme.Expr exposing (Expr(..), bar, foo, foo2)
 
 
 type Expr
@@ -14,3 +14,17 @@ type Expr
     | If Expr Expr Expr
     | Display (List Expr)
     | Quote Expr
+
+
+foo : Expr
+foo =
+    L [ Sym "+", L [ Sym "map", Lambda (L [ Str "x" ]) (L [ Sym "*", Str "x", Str "x" ]), L [ Z 1, Z 2, Z 3, Z 4 ] ] ]
+
+
+foo2 =
+    L (Sym "+" :: bar)
+
+
+bar : List Expr
+bar =
+    [ Sym "map", Lambda (L [ Str "x" ]) (L [ Sym "*", Str "x", Str "x" ]), L [ Z 1, Z 2, Z 3, Z 4 ] ]
