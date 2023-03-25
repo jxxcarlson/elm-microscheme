fa392941b965b2d062433e3f86a57020a3c54241
James Carlson <jxxcarlson@gmail.com>
2023-03-25 14:06:57 -0400

    More examples

--- code/compute-hours.scm
+++ code/compute-hours.scm
@@ -1,11 +1,21 @@
 
 (define my-hours (list (cons 1 20) (cons 2 40)))
-(define (hours-from-hours-minutes h m) (+ h (/ m 60.0)))
+(define (hours-from-hours-minutes h m) (roundTo 2 (+ h (/ m 60.0))))
 (define (hours-from-hours-minutes-pair pair) (hours-from-hours-minutes (car pair) (cdr pair)))
+(define (compute-hours hm-pairs) (map hours-from-hours-minutes-pair hm-pairs))
+(define (compute-earnings rate hm-pairs) (* rate (compute-hours hm-pairs)))
+
 (define (compute-hours-aux hm-pairs) (map hours-from-hours-minutes-pair hm-pairs))
 (define (round2 x) (roundTo 2 x))
 (define (compute-hours hours) (map round2 (compute-hours-aux hours)))
 
+(map (lambda (x) (* 2 x)) 1 2 3) ; oK
+
+(+ (map (lambda (x) (* 2 x)) (1 2 3))); ERROR
+; EvalError 1 "Could not unwrap argument to evalPlus" ,
+; expr (L [Sym "+",L [Sym "map",Lambda (L [Str "x"]) (L [Sym "*",Z 2,Str "x"]),L [Z 1,Z 2,Z 3]]])
+
+foo = (L [Sym "+",L [Sym "map",Lambda (L [Str "x"]) (L [Sym "*",Z 2,Str "x"]),L [Z 1,Z 2,Z 3]]])
 
 ;> (hours-from-hours-minutes 1 30)
 ;1.5
