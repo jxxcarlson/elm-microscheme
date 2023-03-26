a1182c37734f5c500ad3ec67fcd1d6ae8c1efc53
James Carlson <jxxcarlson@gmail.com>
2023-03-25 20:31:22 -0400

    clean up

--- src/MicroScheme/Expr.elm
+++ src/MicroScheme/Expr.elm
@@ -1,4 +1,4 @@
-module MicroScheme.Expr exposing (Expr(..), bar, foo, foo2)
+module MicroScheme.Expr exposing (Expr(..))
 
 
 type Expr
@@ -18,13 +18,4 @@ type Expr
 
 foo : Expr
 foo =
-    L [ Sym "apply", Sym "+", L [ Sym "map", Lambda (L [ Str "x" ]) (L [ Sym "*", Str "x", Str "x" ]), L [ Sym "list", Z 1, Z 2, Z 3, Z 4 ] ] ]
-
-
-foo2 =
-    L (Sym "+" :: bar)
-
-
-bar : List Expr
-bar =
-    [ Sym "map", Lambda (L [ Str "x" ]) (L [ Sym "*", Str "x", Str "x" ]), L [ Z 1, Z 2, Z 3, Z 4 ] ]
+    L [ Sym "cons", Z 2, L [ Sym "+", Z 1, Z 2, Z 3 ] ]
