ce0c9f5239cef96ccd152eb34e518600d48a5ce1
James Carlson <jxxcarlson@gmail.com>
2023-03-27 10:21:45 -0400

    Move code for printing an Expr to module Expr (Expr.print)

--- src/MicroScheme/Expr.elm
+++ src/MicroScheme/Expr.elm
@@ -1,4 +1,4 @@
-module MicroScheme.Expr exposing (Expr(..))
+module MicroScheme.Expr exposing (Expr(..), print)
 
 
 type Expr
@@ -19,3 +19,78 @@ type Expr
 foo : Expr
 foo =
     L [ Sym "cons", Z 2, L [ Sym "+", Z 1, Z 2, Z 3 ] ]
+
+
+
+print : Expr -> String
+print expr =
+    -- expr |> print_ |> prefix "L " |> parens
+    print_ expr
+
+
+prefix : String -> String -> String
+prefix p str =
+    p ++ str
+
+
+print_ : Expr -> String
+print_ expr =
+    case expr of
+        Z n ->
+            String.fromInt n
+
+        F x ->
+            String.fromFloat x
+
+        B b ->
+            if b then
+                "True"
+
+            else
+                "False"
+
+        Str str ->
+            str
+
+        Sym str ->
+            "Sym " ++ quote str
+
+        L exprs ->
+            List.map print exprs |> String.join " " |> parens
+
+        Pair a b ->
+            [ print a, ".", print b ] |> String.join " " |> parens
+
+        Lambda a b ->
+            [ "lambda", print a, print b ] |> String.join " " |> parens
+
+        Define a b ->
+            [ "define", print a, print b ] |> String.join " " |> parens
+
+        If a b c ->
+            [ "if", print a, print b, print c ] |> String.join " " |> parens
+
+        Display exprs ->
+            List.map print exprs |> String.join " "
+
+        Quote expr_ ->
+            print expr_
+
+
+
+-- HELPERS
+
+
+parens : String -> String
+parens str =
+    "(" ++ str ++ ")"
+
+
+bracket : String -> String
+bracket str =
+    "[" ++ str ++ "]"
+
+
+quote : String -> String
+quote str =
+    "\"" ++ str ++ "\""
