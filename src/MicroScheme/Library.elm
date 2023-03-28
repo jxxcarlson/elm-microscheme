70452136bd43860491a5bf939925c9482d111260
James Carlson <jxxcarlson@gmail.com>
2023-03-28 00:12:38 -0400

    Modify library function 'earnings'

--- src/MicroScheme/Library.elm
+++ src/MicroScheme/Library.elm
@@ -5,19 +5,24 @@ import Dict
 
 earningsTotals =
     [ "(define my-hours '((1 30) (1 30)))"
-    , "(define (hours-from-hours-minutes h m) (roundTo 2 (+ h (/ m 60.0))))"
-    , "(define (hours-from-hours-minutes' data) (hours-from-hours-minutes (car data) (car (cdr data))))"
-    , "(define (compute-hours hm-data) (map hours-from-hours-minutes-pair' hm-data))"
-    , "(define (total-hours hours) (apply + (compute-hours hours)))"
-    , "(define (total-earnings rate hours) (* rate (total-hours hours)))"
-    , "(define (earnings-totals rate hours) (cons (total-hours hours) (roundTo 2 (total-earnings rate hours)) ))"
+    , "(define (hours<hm h m) (roundTo 2 (+ h (/ m 60.0))))"
+    , "(define (hours<hm' data) (hours<hm (car data) (car (cdr data))))"
+    , "(define (hours<hm-data hm-data) (map hours<hm' hm-data))"
+    , "(define (total-hours hours) (apply + (hours<hm-data hours)))"
+    , "(define (earnings rate hours) (* rate (total-hours hours)))"
+    , "(define (earnings+hours rate hours) (cons (total-hours hours) (roundTo 2 (earnings rate hours)) ))"
+    , "(define (bar rate hours) (cons 1 (roundTo 2 (earnings rate hours)) ))"
+    , "(define (foo rate hours) (cons 1 (total-hours hours)))"
     ]
         |> String.join ";; "
 
 
 library : Dict.Dict String String
 library =
-    Dict.fromList [ ( "earnings-totals", earningsTotals ) ]
+    Dict.fromList [
+    ( "earnings", earningsTotals )
+    , ( "square", "(define (square x) (* x x))" )
+    ]
 
 
 lookup : String -> Maybe String
